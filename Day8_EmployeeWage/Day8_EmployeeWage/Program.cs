c27e4e8d05d51e68955f6956b447d666216d8478
Arshad <mohammedarshad183@gmail.com>
2022-05-17 12:42:14 +0530

    Displaying Employee wages

---
+++ Day8_EmployeeWage/Day8_EmployeeWage/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8_EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Displaying Welcome to Employee Wage Computation Program on Master Branch");
+            Console.ReadLine();
+        }
+    }
+}
