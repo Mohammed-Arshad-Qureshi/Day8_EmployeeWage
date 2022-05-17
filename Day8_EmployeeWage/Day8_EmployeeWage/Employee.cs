7198e000c8b27bdba9083449cb14f2e03ab24706
Arshad <mohammedarshad183@gmail.com>
2022-05-17 13:19:55 +0530

    Refactoring : by methods and global variables

--- Day8_EmployeeWage/Day8_EmployeeWage/Employee.cs
+++ Day8_EmployeeWage/Day8_EmployeeWage/Employee.cs
@@ -8,12 +8,11 @@ namespace Day8_EmployeeWage
 {
     class Employee
     {
+        int Wage_Per_Hr = 20;
+        int Daily_Wage = 0;
+        int WorkingHrs = 0, WorkingDays = 20, days = 0, Total_Hrs = 0;
         public void Calculate()
         {
-
-            int Wage_Per_Hr = 20;
-            int Daily_Wage = 0;
-            int WorkingHrs = 0, WorkingDays = 20, days = 0, Total_Hrs = 0;
             while (days <= WorkingDays && Total_Hrs <= 100)
             {
                 Random r = new Random();
@@ -31,12 +30,21 @@ namespace Day8_EmployeeWage
                         break;
                 }
                 days++;
+                Console.Write($"day {days} and working hours for the day {WorkingHrs} and salary for the day :   ");
                 Total_Hrs = Total_Hrs + WorkingHrs;
-            }
-            Daily_Wage = Total_Hrs * Wage_Per_Hr;
-            Console.WriteLine("salary : " + Daily_Wage);
-            Console.ReadLine();
 
+                Daily_Wage = Total_Hrs * Wage_Per_Hr;
+                Console.WriteLine(Daily_Wage);
+
+            }
+            display(Daily_Wage);
+
+        }
+
+        public void display(int data)
+        {
+            Console.WriteLine("total monthly salary : " + data);
+            Console.ReadLine();
         }
     }
 }
